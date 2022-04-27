9028d00f14fd87a193569be967ff3c8c82c9c2d2
Simon Duerr <dev@simonduerr.eu>
2022-04-27 17:17:08 +0200

    update metal3d to 0.5 code

--- Metal3D/utils/helpers.py
+++ Metal3D/utils/helpers.py
@@ -1,75 +1,76 @@
-import os 
+import os
 import multiprocessing
 from multiprocessing import Pool
 from turtle import width
 
 import py3Dmol
-import numpy  as np
+import numpy as np
 
 from moleculekit.molecule import Molecule
 from scipy.spatial import KDTree
 from sklearn.cluster import AgglomerativeClustering
 
 
+def create_grid_fromBB(boundingBox, voxelSize=1):
+    """Create a grid from a bounding box.
 
-def create_grid_fromBB(boundingBox, voxelSize = 1):
-    """ Create a grid from a bounding box.
-    
     Parameters
     ----------
     boundingBox : list
         List of the form [xmin, xmax, ymin, ymax, zmin, zmax]
     voxelSize : float
         Size of the voxels in Angstrom
-        
+
     Returns
     -------
     grid : numpy.ndarray
         Grid of shape (nx, ny, nz)
     box_N : numpy.ndarray
         Number of voxels in each dimension
-        
-    """
-    #increase grid by 0.5 to sample everything
-    xrange = np.arange(boundingBox[0][0], boundingBox[1][0]+0.5, step=voxelSize)
-    yrange = np.arange(boundingBox[0][1], boundingBox[1][1]+0.5, step=voxelSize)
-    zrange = np.arange(boundingBox[0][2], boundingBox[1][2]+0.5, step=voxelSize)
 
-    gridpoints = np.zeros((xrange.shape[0]*yrange.shape[0]*zrange.shape[0],3))
+    """
+    # increase grid by 0.5 to sample everything
+    xrange = np.arange(boundingBox[0][0], boundingBox[1][0] + 0.5, step=voxelSize)
+    yrange = np.arange(boundingBox[0][1], boundingBox[1][1] + 0.5, step=voxelSize)
+    zrange = np.arange(boundingBox[0][2], boundingBox[1][2] + 0.5, step=voxelSize)
+
+    gridpoints = np.zeros((xrange.shape[0] * yrange.shape[0] * zrange.shape[0], 3))
     i = 0
     for x in xrange:
         for y in yrange:
             for z in zrange:
-                gridpoints[i][0]=x
-                gridpoints[i][1]=y
-                gridpoints[i][2]=z
-                i+=1
+                gridpoints[i][0] = x
+                gridpoints[i][1] = y
+                gridpoints[i][2] = z
+                i += 1
     return gridpoints, (xrange.shape[0], yrange.shape[0], zrange.shape[0])
+
 
 def get_bb(points):
     """Return bounding box from a set of points (N,3)
-    
+
     Parameters
     ----------
     points : numpy.ndarray
         Set of points (N,3)
-    
+
     Returns
     -------
     boundingBox : list
         List of the form [xmin, xmax, ymin, ymax, zmin, zmax]
-    
+
     """
-    minx = np.min(points[:,0])
-    maxx = np.max(points[:,0])
+    minx = np.min(points[:, 0])
+    maxx = np.max(points[:, 0])
 
-    miny = np.min(points[:,1])
-    maxy = np.max(points[:,1])
+    miny = np.min(points[:, 1])
+    maxy = np.max(points[:, 1])
 
-    minz = np.min(points[:,2])
-    maxz = np.max(points[:,2])
-    bb = [[minx, miny, minz],[maxx, maxy, maxz]]
+    minz = np.min(points[:, 2])
+    maxz = np.max(points[:, 2])
+    bb = [[minx, miny, minz], [maxx, maxy, maxz]]
     return bb
+
 
 def get_all_protein_resids(pdb_file):
     """Return all protein residues from a pdb file
@@ -88,10 +89,10 @@ def get_all_protein_resids(pdb_file):
     try:
         prot = Molecule(pdb_file)
     except:
-        exit('could not read file')
-    prot.filter('protein')
-    mapping = prot.renumberResidues(returnMapping=True)
-    return np.array(mapping['new_resid'])
+        exit("could not read file")
+    prot.filter("protein")
+    return prot.get("index", sel="name CA")
+
 
 def get_all_metalbinding_resids(pdb_file):
     """Return all metal binding residues from a pdb file
@@ -111,34 +112,40 @@ def get_all_metalbinding_resids(pdb_file):
     try:
         prot = Molecule(pdb_file)
     except:
-        exit('could not read file')
-    prot.filter('protein')
-    prot.renumberResidues()
-    return prot.get('resid', sel="name CA and resname HIS HID HIE HIP CYS CYX GLU GLH GLN ASP ASH ASN GLN MET")
+        exit("could not read file")
+    prot.filter("protein")
+    return prot.get(
+        "index",
+        sel="name CA and resname HIS HID HIE HIP CYS CYX GLU GLH GLN ASP ASH ASN GLN MET",
+    )
+
 
 def compute_average_p_fast(point, cutoff=1):
     """Using KDTree find the closest gridpoints
-    
+
     Parameters
     ----------
     point : numpy.ndarray
         Point of shape (3,)
     cutoff : float
         Cutoff distance in Angstrom
-    
+
     Returns
     -------
     average_p : numpy.ndarray
         Average probability of shape (1,)"""
     p = 0
-    nearest_neighbors,indices = tree.query(point,k=15, distance_upper_bound=cutoff, workers=1)
-    if np.min(nearest_neighbors)!=np.inf:
-       p = np.mean(output_v[indices[nearest_neighbors!=np.inf]])
+    nearest_neighbors, indices = tree.query(
+        point, k=15, distance_upper_bound=cutoff, workers=1
+    )
+    if np.min(nearest_neighbors) != np.inf:
+        p = np.mean(output_v[indices[nearest_neighbors != np.inf]])
     return p
+
 
 def get_probability_mean(grid, prot_centers, pvalues):
     """Compute the mean probability of all gridpoints from the globalgrid based on the individual boxes
-    
+
     Parameters
     ----------
     grid : numpy.ndarray
@@ -147,7 +154,7 @@ def get_probability_mean(grid, prot_centers, pvalues):
         Protein centers of shape (N,3)
     pvalues : numpy.ndarray
         Probability values of shape (N,1)
-    
+
     Returns
     -------
     mean_p : numpy.ndarray
@@ -157,18 +164,19 @@ def get_probability_mean(grid, prot_centers, pvalues):
     output_v = pvalues
     global prot_v
     prot_v = prot_centers
-    cpuCount=multiprocessing.cpu_count()
+    cpuCount = multiprocessing.cpu_count()
 
-    global tree 
+    global tree
     tree = KDTree(prot_v)
     p = Pool(cpuCount)
     results = p.map(compute_average_p_fast, grid)
     return np.array(results)
 
-def write_cubefile(bb, pvalues, box_N, outname='Metal3D_pmap.cube'):
+
+def write_cubefile(bb, pvalues, box_N, outname="Metal3D_pmap.cube", gridres=1):
     """Write a cube file from a probability map
     The cube specification from gaussian is used, distance are converted to bohr
-    
+
     Parameters
     ----------
     bb : list
@@ -179,18 +187,28 @@ def write_cubefile(bb, pvalues, box_N, outname='Metal3D_pmap.cube'):
         Number of voxels in each dimension
     outname : str
         Name of the output file
-    
+    gridres:float
+        Resolution of the grid used for writing the voxels
+
     """
 
-    with open(outname, 'w') as cube:
-        cube.write(' Metal3D Cube File\n')
-        cube.write(' Outer Loop: X, Middle Loop y, inner Loop z\n')
-    
-        angstromToBohr =1.89
-        cube.write(f'    1   {bb[0][0]*angstromToBohr: .6f}  {bb[0][1]*angstromToBohr: .6f}   {bb[0][2]*angstromToBohr: .6f}\n')
-        cube.write(f"{str(box_N[0]).rjust(5)}    1.890000    0.000000    0.000000\n")
-        cube.write(f"{str(box_N[1]).rjust(5)}    0.000000    1.890000    0.000000\n")
-        cube.write(f"{str(box_N[2]).rjust(5)}    0.000000    0.000000    1.890000\n")
+    with open(outname, "w") as cube:
+        cube.write(" Metal3D Cube File\n")
+        cube.write(" Outer Loop: X, Middle Loop y, inner Loop z\n")
+
+        angstromToBohr = 1.89
+        cube.write(
+            f"    1   {bb[0][0]*angstromToBohr: .6f}  {bb[0][1]*angstromToBohr: .6f}   {bb[0][2]*angstromToBohr: .6f}\n"
+        )
+        cube.write(
+            f"{str(box_N[0]).rjust(5)}    {1.890000*gridres:.9f}    0.000000    0.000000\n"
+        )
+        cube.write(
+            f"{str(box_N[1]).rjust(5)}    0.000000    {1.890000*gridres:.9f}    0.000000\n"
+        )
+        cube.write(
+            f"{str(box_N[2]).rjust(5)}    0.000000    0.000000    {1.890000*gridres:.9f}\n"
+        )
         cube.write("    1    1.000000    0.000000    0.000000    0.000000\n")
 
         o = pvalues.reshape(box_N)
@@ -199,15 +217,18 @@ def write_cubefile(bb, pvalues, box_N, outname='Metal3D_pmap.cube'):
                 for z in range(box_N[2]):
                     cube.write(f" {o[x][y][z]: .5E}")
                     if z % 6 == 5:
-                        cube.write('\n')
-                cube.write('\n')
+                        cube.write("\n")
+                cube.write("\n")
 
-def find_unique_sites(pvalues, grid, writeprobes=False, probefile='probes.pdb', threshold=5, p=0.75):
+
+def find_unique_sites(
+    pvalues, grid, writeprobes=False, probefile="probes.pdb", threshold=5, p=0.75
+):
     """The probability voxels are points and the voxel clouds may contain multiple metals
     This function finds the unique sites and returns the coordinates of the unique sites with the highest p for each cluster.
     It uses the AgglomerativeClustering algorithm to find the unique sites.
     The threshold is the maximum distance between two points in the same cluster it can be changed to get more metal points.
-    
+
     Parameters
     ----------
     pvalues : numpy.ndarray
@@ -222,37 +243,46 @@ def find_unique_sites(pvalues, grid, writeprobes=False, probefile='probes.pdb', 
         Maximum distance between two points in the same cluster
     p : float
         Minimum probability of a point to be considered a unique site
-    
+
     """
-    
-    points=grid[pvalues>p]
-    point_p = pvalues[pvalues>p]
-    if len(points)==0:
-        print('no points available for clustering, no probes will be written')
+
+    points = grid[pvalues > p]
+    point_p = pvalues[pvalues > p]
+    if len(points) == 0:
+        print("no points available for clustering, no probes will be written")
         return None
-    clustering = AgglomerativeClustering(n_clusters=None,linkage="complete", distance_threshold=threshold).fit(points)
-    
-    print(f'p={p}, n(metals):', clustering.n_clusters_)
+    clustering = AgglomerativeClustering(
+        n_clusters=None, linkage="complete", distance_threshold=threshold
+    ).fit(points)
+
+    print(f"p={p}, n(metals):", clustering.n_clusters_)
 
     sites = []
     for i in range(clustering.n_clusters_):
-        c_points = points[clustering.labels_==i]
-        c_points_p = point_p[clustering.labels_==i]
+        c_points = points[clustering.labels_ == i]
+        c_points_p = point_p[clustering.labels_ == i]
 
         position = c_points[np.argmax(c_points_p)]
         sites.append((position, np.max(c_points_p)))
     if writeprobes:
-        print(f'writing probes to {probefile}')
-        with open(probefile, 'w') as f:
-            for i,site in enumerate(sites):
-                 f.write(f'HETATM    1 ZN    ZN A {i:3}    {site[0][0]: 8.3f}{site[0][1]: 8.3f}{site[0][2]: 8.3f}  {site[1]:.2f}  0.0           ZN2+\n')
+        print(f"writing probes to {probefile}")
+        with open(probefile, "w") as f:
+            for i, site in enumerate(sites):
+                f.write(
+                    f"HETATM  {i+1:3} ZN    ZN A {i+1:3}    {site[0][0]: 8.3f}{site[0][1]: 8.3f}{site[0][2]: 8.3f}  {site[1]:.2f}  0.0           ZN2+\n"
+                )
 
 
+def show_map(
+    pdb,
+    p=0.5,
+    show_sticks_all=False,
+    show_sticks_metalbinding=True,
+    show_probes=True,
+    show_pdb_metals=True,
+):
+    """Show the probability map of a protein and the protein using py3Dmol
 
-
-def show_map(pdb,p=0.5, show_sticks_all=False, show_sticks_metalbinding=True, show_probes=True, show_pdb_metals=True):
-    """ Show the probability map of a protein and the protein using py3Dmol
-    
     Parameters
     ----------
     pdb : str
@@ -263,77 +293,94 @@ def show_map(pdb,p=0.5, show_sticks_all=False, show_sticks_metalbinding=True, sh
         If True, show all the residues as sticks in the protein
     show_sticks_metalbinding : bool
         If True, show the residues that are metal binding as sticks in the protein
-    
+
     Returns
     -------
     view : py3Dmol.view
         The view of the protein and the probability map
     """
-    view=py3Dmol.view(width=1000, height=800)
+    view = py3Dmol.view(width=1000, height=800)
 
-    view.addModel(open(pdb+'.pdb', 'r').read(),'pdb')
+    view.addModel(open(pdb + ".pdb", "r").read(), "pdb")
     if show_probes:
-        view.addModel(open('probes_'+pdb+'.pdb', 'r').read(),'pdb')
-        probes = open('probes_'+pdb+'.pdb', 'r').readlines()
-        probabilites = [float(p[55:60]) for p in probes] # read p from occupancy column
+        view.addModel(open("probes_" + pdb + ".pdb", "r").read(), "pdb")
+        probes = open("probes_" + pdb + ".pdb", "r").readlines()
+        probabilites = [float(p[55:60]) for p in probes]  # read p from occupancy column
         colors = {}
         # use different colors for the probabilities
-        for i,x in enumerate(probabilites):
-            colors[i] = '#%02x%02x%02x' % (0, 0, int(x*255)/2)
+        for i, x in enumerate(probabilites):
+            colors[i] = "#%02x%02x%02x" % (0, 0, int(x * 255 / 2))
 
-    # check file size of cube file 
-    if os.path.getsize('metal_'+pdb+'.cube') < 5000000:
-        view.addVolumetricData(open('metal_'+pdb+'.cube', 'r').read(),"cube", {'isoval': p, 'color': "blue", 'opacity': 0.75})
+    # check file size of cube file
+    if os.path.getsize("metal_" + pdb + ".cube") < 10000000:
+        view.addVolumetricData(
+            open("metal_" + pdb + ".cube", "r").read(),
+            "cube",
+            {"isoval": p, "color": "blue", "opacity": 0.75},
+        )
     else:
-        print('cube file is too big, not showing the map')
-        print('Use the VMD programm to visualize the map or choose custom residues in close proximity')
-    
+        print("cube file is too big, not showing the map")
+        print(
+            "Use smaller grid resolution or choose custom residues in close proximity to reduce file size. \n Alternatively, view full size cube file in VMD or UCSF ChimeraX."
+        )
+
     view.zoomTo()
-    view.setBackgroundColor('white')
-    view.setStyle({},{'cartoon': {'color':'gray'}})
+    view.setBackgroundColor("white")
+    view.setStyle({}, {"cartoon": {"color": "gray"}})
     if show_sticks_all:
-        view.setStyle({}, {'stick':{},'cartoon': {'color':'gray'}})
+        view.setStyle({}, {"stick": {}, "cartoon": {"color": "gray"}})
     if show_pdb_metals:
-        view.getModel(0).setStyle({'resn':"ZN"},{'sphere': {}})
-        view.getModel(0).setStyle({'resn':"CA"},{'sphere': {}})
-        view.getModel(0).setStyle({'resn':"CU"},{'sphere': {}})
-        view.getModel(0).setStyle({'resn':"HG"},{'sphere': {}})
-        view.getModel(0).setStyle({'resn':"MG"},{'sphere': {}})
-        view.getModel(0).setStyle({'resn':"FE"},{'sphere': {}})
-        view.getModel(0).setStyle({'resn':"MN"},{'sphere': {}})
-        view.getModel(0).setStyle({'resn':"NI"},{'sphere': {}})
-        view.getModel(0).setStyle({'resn':"MB"},{'sphere': {}})
-    
+        view.getModel(0).setStyle({"resn": "ZN"}, {"sphere": {}})
+        view.getModel(0).setStyle({"resn": "CA"}, {"sphere": {}})
+        view.getModel(0).setStyle({"resn": "CU"}, {"sphere": {}})
+        view.getModel(0).setStyle({"resn": "HG"}, {"sphere": {}})
+        view.getModel(0).setStyle({"resn": "MG"}, {"sphere": {}})
+        view.getModel(0).setStyle({"resn": "FE"}, {"sphere": {}})
+        view.getModel(0).setStyle({"resn": "MN"}, {"sphere": {}})
+        view.getModel(0).setStyle({"resn": "NI"}, {"sphere": {}})
+        view.getModel(0).setStyle({"resn": "MB"}, {"sphere": {}})
+
     if show_probes:
-        view.getModel(1).setStyle({'resn':"ZN"},{'sphere': {'colorscheme':{'prop':'index', 'map':colors}}})
-    
+        view.getModel(1).setStyle(
+            {"resn": "ZN"},
+            {"sphere": {"colorscheme": {"prop": "index", "map": colors}}},
+        )
+
     # add hoverable labels for the residues and the predicted metals
     # two callbacks are needed, one for the residues and one for the metals
     # the metal one displays the probability
-    view.getModel(0).setHoverable({},True,'''function(atom,viewer,event,container) {
+    view.getModel(0).setHoverable(
+        {},
+        True,
+        """function(atom,viewer,event,container) {
                     if(!atom.label) {
                         atom.label = viewer.addLabel(atom.resn+atom.resi+":"+atom.atom,{position: atom, backgroundColor: 'mintcream', fontColor:'black'});
-                    }}''',
-                '''function(atom,viewer) { 
+                    }}""",
+        """function(atom,viewer) { 
                     if(atom.label) {
                         viewer.removeLabel(atom.label);
                         delete atom.label;
                     }
-                    }''')
-    view.getModel(1).setHoverable({},True,'''function(atom,viewer,event,container) {
+                    }""",
+    )
+    view.getModel(1).setHoverable(
+        {},
+        True,
+        """function(atom,viewer,event,container) {
                     if(!atom.label) {
                         atom.label = viewer.addLabel('ZN p='+atom.pdbline.substring(55,60),{position: atom, backgroundColor: 'mintcream', fontColor:'black'});
-                    }}''',
-                '''function(atom,viewer) { 
+                    }}""",
+        """function(atom,viewer) { 
                     if(atom.label) {
                         viewer.removeLabel(atom.label);
                         delete atom.label;
                     }
-                    }''')
+                    }""",
+    )
     if show_sticks_metalbinding:
-        view.setStyle({'resn':"HIS"},{'stick': {}, 'cartoon': {'color':'gray'}})
-        view.setStyle({'resn':"ASP"},{'stick': {}, 'cartoon': {'color':'gray'}})
-        view.setStyle({'resn':"GLU"},{'stick': {}, 'cartoon': {'color':'gray'}})
-        view.setStyle({'resn':"CYS"},{'stick': {}, 'cartoon': {'color':'gray'}})
+        view.setStyle({"resn": "HIS"}, {"stick": {}, "cartoon": {"color": "gray"}})
+        view.setStyle({"resn": "ASP"}, {"stick": {}, "cartoon": {"color": "gray"}})
+        view.setStyle({"resn": "GLU"}, {"stick": {}, "cartoon": {"color": "gray"}})
+        view.setStyle({"resn": "CYS"}, {"stick": {}, "cartoon": {"color": "gray"}})
 
     return view.show()
--- Metal3D/utils/voxelization.py
+++ Metal3D/utils/voxelization.py
@@ -3,6 +3,7 @@ import multiprocessing
 from multiprocessing import Pool
 
 import torch
+import numpy as np
 
 from moleculekit.molecule import Molecule
 from moleculekit.tools.voxeldescriptors import getVoxelDescriptors
@@ -13,19 +14,48 @@ from moleculekit.tools.preparation import systemPrepare
 class AtomtypingError(Exception):
     pass
 
+
 class StructureCleaningError(Exception):
     pass
+
 
 class ProteinPrepareError(Exception):
     pass
 
+
 class VoxelizationError(Exception):
     pass
 
-metal_atypes = ('MG', 'ZN', 'MN', 'CA', 'FE', 'HG', 'CD', 'NI', 'CO', 'CU', 'K', 'LI', 'Mg', 'Zn', 'Mn', 'Ca', 'Fe', 'Hg', 'Cd', 'Ni', 'Co', 'Cu', 'Li')
+
+metal_atypes = (
+    "MG",
+    "ZN",
+    "MN",
+    "CA",
+    "FE",
+    "HG",
+    "CD",
+    "NI",
+    "CO",
+    "CU",
+    "K",
+    "LI",
+    "Mg",
+    "Zn",
+    "Mn",
+    "Ca",
+    "Fe",
+    "Hg",
+    "Cd",
+    "Ni",
+    "Co",
+    "Cu",
+    "Li",
+)
+
 
 def voxelize_single_notcentered(env):
-    """voxelize 1 structure, executed on a single CPU 
+    """voxelize 1 structure, executed on a single CPU
     Using 7 of the 8 channels supplied by moleculekit(excluding metals)
     Additionally it uses all the metalbinding residues as channel
 
@@ -47,34 +77,81 @@ def voxelize_single_notcentered(env):
     """
     prot, id = env
 
-    c = prot.get('coords',sel=f"resid {id} and name CA")
+    c = prot.get("coords", sel=f"index {id} and name CA")
 
-    size=[20,20,20] # size of box
-    voxels = torch.zeros(8,20,20,20)
+    size = [16, 16, 16]  # size of box
+    voxels = torch.zeros(8, 32, 32, 32)
 
     try:
-        prot_vox, prot_centers, prot_N = getVoxelDescriptors(prot, buffer=1, center=c, boxsize=size,method="C", validitychecks=False)
-    except:
-        return None
-    nchannels = prot_vox.shape[1]
-    prot_vox_t = prot_vox.transpose().reshape([1, nchannels, prot_N[0], prot_N[1], prot_N[2]]).copy()
-    metalcoordination = prot.atomselect('(resname HID HIS HIE HIP ASP ASH GLU GLH CYS and sidechain) or (protein and name O N)')
-    metalcoordination = metalcoordination.reshape(metalcoordination.shape[0],1)
-    coord_vox, _, _ = getVoxelDescriptors(prot, center=c, voxelsize=1, userchannels=metalcoordination, boxsize=[20,20,20], method="C", validitychecks=False)
-    coord_vox_t = coord_vox.transpose().reshape([1, prot_N[0], prot_N[1], prot_N[2]])
-    
-    prot_vox_t = torch.from_numpy(prot_vox_t)
-    indices = torch.LongTensor([0,1,2,3,4,5,7])
-    voxels[0:7]=torch.index_select(prot_vox_t,1, indices)
+        hydrophobic = prot.atomselect("element C")
+        hydrophobic = hydrophobic.reshape(hydrophobic.shape[0], 1)
 
-    voxels[7]=torch.from_numpy(coord_vox_t)
-    
+        aromatic = prot.atomselect(
+            "resname HIS HIE HIP HID TRP TYR PHE and sidechain and not name CB and not hydrogen"
+        )
+        aromatic = aromatic.reshape(aromatic.shape[0], 1)
+
+        metalcoordination = prot.atomselect(
+            "(name ND1 NE2 SG OE1 OE2 OD2) or (protein and name O N)"
+        )
+        metalcoordination = metalcoordination.reshape(metalcoordination.shape[0], 1)
+
+        hbondacceptor = prot.atomselect(
+            "(resname ASP GLU HIS HIE HIP HID SER THR MSE CYS MET and name ND2 NE2 OE1 OE2 OD1 OD2 OG OG1 SE SG) or name O"
+        )
+        hbondacceptor = hbondacceptor.reshape(metalcoordination.shape[0], 1)
+
+        hbonddonor = prot.atomselect(
+            "(resname ASN GLN ASH GLH TRP MSE SER THR MET CYS and name ND2 NE2 NE1 SG SE OG OG1) or name N"
+        )
+        hbonddonor = hbonddonor.reshape(metalcoordination.shape[0], 1)
+
+        positive = prot.atomselect(
+            "resname LYS ARG HIS HIE HIP HID and name NZ NH1 NH2 ND1 NE2 NE"
+        )
+        positive = positive.reshape(positive.shape[0], 1)
+
+        negative = prot.atomselect("(resname ASP GLU ASH GLH and name OD1 OD2 OE1 OE2)")
+        negative = negative.reshape(negative.shape[0], 1)
+
+        occupancy = prot.atomselect("protein and not hydrogen")
+        occupancy = occupancy.reshape(occupancy.shape[0], 1)
+        userchannels = np.hstack(
+            [
+                hydrophobic,
+                aromatic,
+                metalcoordination,
+                hbondacceptor,
+                hbonddonor,
+                positive,
+                negative,
+                occupancy,
+            ]
+        )
+        prot_vox, prot_centers, prot_N = getVoxelDescriptors(
+            prot,
+            center=c,
+            userchannels=userchannels,
+            boxsize=size,
+            voxelsize=0.5,
+            validitychecks=False,
+        )
+    except:
+        raise VoxelizationError(f"voxelization of {id} failed")
+    nchannels = prot_vox.shape[1]
+    prot_vox_t = (
+        prot_vox.transpose()
+        .reshape([1, nchannels, prot_N[0], prot_N[1], prot_N[2]])
+        .copy()
+    )
+
+    voxels = torch.from_numpy(prot_vox_t)
     return (voxels, prot_centers, prot_N, prot.copy())
 
 
-def processStructures(pdb_file, resids,clean=True):
+def processStructures(pdb_file, resids, clean=True):
     """Process a pdb file and return a list of voxelized boxes centered on the residues
-    
+
     Parameters
     ----------
     pdb_file : str
@@ -83,77 +160,49 @@ def processStructures(pdb_file, resids,clean=True):
         List of resids to center the voxels on
     clean : bool
         If True, remove all non-protein residues from the pdb file
-    
+
     Returns
     -------
     voxels : torch.Tensor
-        Voxelized boxes with 8 channels (N, 8,20,20,20)
+        Voxelized boxes with 8 channels (N, 8,32,32,32)
     prot_centers_list : list
-        List of the centers of the voxels (N*20**20*20,3)
+        List of the centers of the voxels (N*32**32*32,3)
     prot_n_list : list
         List of the number of voxels in each box (N,3)
     envs: list
         List of tuples (prot, idx) (N)
     """
-    
+
     start_time_processing = time.time()
 
     # load molecule using MoleculeKit
     try:
         prot = Molecule(pdb_file)
     except:
-        raise IOError('could not read pdbfile')
+        raise IOError("could not read pdbfile")
 
     if clean:
-        prot.filter('protein')
-        prot.renumberResidues()
-        
-        # prepare for atomtyping
-        try:
-            # prot= proteinPrepare(prot) switch to new systemprepare
-            prot= systemPrepare(prot, titration=False, verbose=False)
-            prot.filter("protein or water or element {}".format(" ".join(metal_atypes))) #prot.remove("name OXT H2 H3") # this is a necessary hack because otherwise atomtyping might fail
-        except: 
-            raise StructureCleaningError('structure could not be cleaned')
+        prot.filter("protein and not hydrogen")
 
-    try:
-        # prot = prepareProteinForAtomtyping(prot)
-        prot = prepareProteinForAtomtyping(prot, protonate=False, verbose=False)
-        prot.filter("protein or water or element {}".format(" ".join(metal_atypes)))
-    except: 
-        raise ProteinPrepareError('failed to prepare protein for atomtyping')
-
-   
     environments = []
     for idx in resids:
         try:
             environments.append((prot.copy(), idx))
         except:
-            print('ignoring '+idx)
-
-    
+            print("ignoring " + idx)
 
     prot_centers_list = []
     prot_n_list = []
     envs = []
 
-    cpuCount=multiprocessing.cpu_count()
-    p = Pool(cpuCount)
+    results = [voxelize_single_notcentered(x) for x in environments]
 
-    results = p.map(voxelize_single_notcentered, environments)  # prot_centers, prot_N
+    voxels = torch.empty(len(results), 8, 32, 32, 32, device="cuda")
 
-    # remove None results
-    results = [x for x in results if x is not None]
+    vox_env, prot_centers_list, prot_n_list, envs = zip(*results)
 
-    voxels=torch.empty(len(results),8,20,20,20)
-
-    if len(results)==0:
-        raise VoxelizationError('something went wrong with the voxelization, check that there are no discontinuities in the protein')
-
-    vox_env, prot_centers_list, prot_n_list, envs = zip(*results) 
-
-    for i,vox_env in enumerate(vox_env):
-        voxels[i]=vox_env
+    for i, vox_env in enumerate(vox_env):
+        voxels[i] = vox_env
 
     print(f"Voxelization took  {time.time() - start_time_processing:.3f} seconds ")
 
